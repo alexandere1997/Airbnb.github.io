0c427eed2de0ae3f1adc153fc3407a3b5f60cdbb
Alexander <Sanyi.Ponzel@mail.ru>
2019-07-12 18:00:29 +0700

    тест 6

--- assets/js/scrollMenu.js
+++ assets/js/scrollMenu.js
@@ -1,3 +1,7 @@
 $(window).scroll(function(){
-  $('nav').toggleClass('bg-white header__line fixed-top', $(this).scrollTop() > 725);
+  $('nav').toggleClass('bg-white header__line fixed-top', $(this).scrollTop() > 1);
+});
+
+$(window).scroll(function(){
+  $('.navbar-toggler').attr({"data-toggle":"collapse"} , $(this).scrollTop() > 1);
 });
